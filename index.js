f9ac049d78d11af4dac8f7f3f004e024294eee91
AlejandroAn12 <negocios1219@gmail.com>
2020-09-27 17:01:56 -0500

    Cambios sobre bandas

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
-//APP EXPRESS
-const app = express();
-const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
-//NODE SERVER
+// App de Express
+const app = express();
+
+// Node Server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-//PathPublic
-const publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
 
-server.listen(PORT, (err) => {
 
-    if(err) throw new Error(err);
-    console.log('Servidor corriendo en puerto', 3000);
+
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
 
 });
+
+
